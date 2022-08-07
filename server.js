767aaec7752edef81eea23d5fc8aa853d8c6ec8c
andreyanez <andre.yanez22@gmail.com>
2022-08-07 13:08:05 -0500

    added heroku config

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const bucketListItemRoutes = require('./routes/api/bucketListItems');
+const path = require('path');
 
 app.use(cors());
 app.use(morgan('tiny'));
@@ -22,6 +23,13 @@ mongoose
 	.catch(err => console.log(err));
 
 app.use('/api/bucketListItems', bucketListItemRoutes);
-app.get('/', (req, res) => res.send('Hello'));
+// app.get('/', (req, res) => res.send('Hello'));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/dist'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+	});
+}
 
 app.listen(PORT, () => console.log(`App on port http://localhost:${PORT}`));
